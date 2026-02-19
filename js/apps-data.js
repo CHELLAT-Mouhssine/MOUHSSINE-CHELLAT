991aec13a807cb1c961b0764b7686175f9bf9b64
CHELLAT-Mouhssine <leonardo.b2026@gmail.com>
2026-02-19 16:37:09 +0000

    Update apps-data.js

--- js/apps-data.js
+++ js/apps-data.js
@@ -1,1 +1,13 @@
 
+const appsData = [
+    {
+        id: 1,
+        name: "Fifa 2026",
+        image: "assets/images/icons/Fifa.jpg",
+        link: "رابط_الربح_الخاص_بك",
+        category: "Games",
+        description: "أحدث نسخة معدلة من فيفا"
+    },
+    // كرر هذا لكل تطبيق من الـ 44
+];
+export default appsData;
